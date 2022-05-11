e738bba55a0854c11e4670f1556e3235a9021293
Cemil Ülay <cemil@MacBook-Air-som-tillhor-Cemil.local>
2022-05-11 14:11:13 +0200

    add footer component and in pages

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <h1>Home</h1>
+      <Footer />
     </div>
   );
 };
