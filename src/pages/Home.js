af54858ef26043bae9da389470cea29516caf4d7
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-05-13 13:00:05 +0200

    Add sign up button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,17 @@
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div>
       <Header />
       <h1>Home</h1>
+      <Link to="/signup">
+        <button className="bg-purple-500 hover:bg-purple-700 text-white py-2 px-9 rounded-full">
+          Sign up
+        </button>
+      </Link>
       <Footer />
     </div>
   );
