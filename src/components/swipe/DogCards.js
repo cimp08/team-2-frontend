7a6e41466a3286b148b402e39435a9547256629d
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-05-18 11:40:29 +0200

    Change some styles for dog cards component

--- src/components/swipe/DogCards.js
+++ src/components/swipe/DogCards.js
@@ -2,29 +2,32 @@ import React from "react";
 import "./DogCards.css";
 import TinderCard from "react-tinder-card";
 import { useState } from "react";
-import { YoutubeSearchedForSharp } from "@mui/icons-material";
+import { List } from "@mui/material";
 
 const DogCards = () => {
 	const dogs = [
 		{
-			name: "Snow",
+			name: "Cassy",
 			age: "5 years",
-			breed: "something",
-			info: "He likes to hunt cats all day.",
+			breed: "Bulldog",
+			gender: "female",
+			about: "He likes to hunt cats all day.",
 			url: "https://images.unsplash.com/photo-1583512603805-3cc6b41f3edb?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8ZG9nfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60",
 		},
 		{
-			name: "White",
+			name: "Charlie",
 			age: "2 years",
-			breed: "something",
-			info: "He likes to eat vegetables all day.",
+			breed: "Labrador",
+			gender: "female",
+			about: "He likes to eat vegetables all day.",
 			url: "https://images.unsplash.com/photo-1588943211346-0908a1fb0b01?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
 		},
 		{
-			name: "Black",
+			name: "Max",
 			age: "9 years",
-			breed: "something",
-			info: "He likes to sleep all.",
+			breed: "French bulldog",
+			gender: "male",
+			about: "He likes to sleep all day.",
 			url: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGRvZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60",
 		},
 	];
@@ -39,8 +42,8 @@ const DogCards = () => {
 	};
 
 	return (
-		<div className="swipe_container w-3/4 flex flex-col justify-center content-center h-full">
-			<div className="card_container w-80 h-96">
+		<div className="flex flex-col justify-center content-center h-full">
+			<div className="w-72 h-96">
 				{dogs.map((dog) => (
 					<TinderCard
 						className="swipe absolute"
@@ -50,14 +53,21 @@ const DogCards = () => {
 					>
 						<div
 							style={{ backgroundImage: `url(${dog.url})` }}
-							className="card w-80 h-96 shadow-xl"
+							className="w-72 h-96 shadow-xl bg-cover bg-center rounded-3xl"
 						>
-				
-							<div className="info_container bg-white w-64 h-32 rounded-3xl absolute text-xs shadow-2xl">
-								<p>{dog.name}</p>
-								<p>{dog.age}</p>
-								<p>{dog.breed}</p>
-								<p>{dog.info}</p>
+							<div className="info_container bg-white w-64 h-28 p-4 rounded-3xl absolute text-xs shadow-2xl">
+								<p className="text-sm font-semibold">{dog.name}</p>
+								<List className="list-none">
+									<li>
+										<p>{dog.age}</p>
+									</li>
+									<li>
+										<p>{dog.breed}</p>
+									</li>
+									<li>
+										<p>{dog.about}</p>
+									</li>
+								</List>
 							</div>
 						</div>
 					</TinderCard>
