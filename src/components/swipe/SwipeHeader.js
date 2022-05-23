8dd675b06072061f19892adc4d54153d06cc4ca2
HeidiDragomir <marinela.dragomir@chasacademy.se>
2022-05-23 17:06:43 +0200

    Add home icon and some styles to make responsive

--- src/components/swipe/SwipeHeader.js
+++ src/components/swipe/SwipeHeader.js
@@ -3,25 +3,30 @@ import { IconButton } from "@mui/material";
 import PetsIcon from "@mui/icons-material/Pets";
 import SettingsIcon from "@mui/icons-material/Settings";
 import ChatIcon from "@mui/icons-material/Chat";
+import HomeIcon from "@mui/icons-material/Home";
 import "./SwipeHeader.css";
 
 const SwipeHeader = () => {
 	return (
-		<div className="swipe_header left-28 flex flex-col justify-between absolute">
-			<div className="icon mb-40">
-				<IconButton className="swipe_paw">
+		<div className="swipe_header flex md:flex-col justify-between pb-12 md:left-28 md:absolute">
+			<div className="icon md:mb-20">
+				<IconButton className="swipe_icons">
 					<PetsIcon fontSize="large"></PetsIcon>
 				</IconButton>
 			</div>
-
-			<div className="icon mb-40">
-				<IconButton className="swipe_settings">
+			<div className="icon md:mb-20">
+				<IconButton className="swipe_icons">
 					<SettingsIcon fontSize="large"></SettingsIcon>
 				</IconButton>
 			</div>
-			<div className="">
-				<IconButton className="swipe_chat">
+			<div className="icon md:mb-20">
+				<IconButton className="swipe_icons">
 					<ChatIcon fontSize="large"></ChatIcon>
+				</IconButton>
+			</div>
+			<div>
+				<IconButton className="swipe_icons">
+					<HomeIcon fontSize="large"></HomeIcon>
 				</IconButton>
 			</div>
 		</div>
